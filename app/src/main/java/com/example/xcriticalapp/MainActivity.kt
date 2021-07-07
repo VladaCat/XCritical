855715e0cd7703a6f58a0d8e3d1b8559d9fa53ab
Vlada Dorozhko <dorozhko2v@gmail.com>
2021-07-07 16:10:47 +0300

    add Error processing

--- app/src/main/java/com/example/xcriticalapp/MainActivity.kt
+++ app/src/main/java/com/example/xcriticalapp/MainActivity.kt
@@ -2,9 +2,17 @@ package com.example.xcriticalapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View;
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+
+lateinit var sign_in_button:Button
+lateinit var registration_button:Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +22,65 @@ class MainActivity : AppCompatActivity() {
         val password by lazy {findViewById<EditText>(R.id.password_editText)}
         val wrong_email by lazy {findViewById<TextView>(R.id.wrong_email_text)}
         val wrong_password by lazy {findViewById<TextView>(R.id.wrong_password_text)}
+
+        sign_in_button = findViewById(R.id.sign_in_button)
+        registration_button = findViewById(R.id.register_button)
+
+        sign_in_button.setOnClickListener {
+            if(email.text.toString().isEmpty())
+            {
+                wrong_email.visibility=View.VISIBLE
+            }
+            if(password.text.toString().isEmpty())
+            {
+                wrong_password.visibility=View.VISIBLE
+            }
+        }
+
+        registration_button.setOnClickListener {
+            if(email.text.toString().isEmpty())
+            {
+                wrong_email.visibility=View.VISIBLE
+            }
+            if(password.text.toString().isEmpty())
+            {
+                wrong_password.visibility=View.VISIBLE
+            }
+        }
+
+        email.addTextChangedListener(object:TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                wrong_email.visibility=View.INVISIBLE
+            }
+        })
+
+        password.addTextChangedListener(object:TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                wrong_password.visibility=View.INVISIBLE
+            }
+        })
     }
 
 }
