0eae5feac90e509b63f28e9fdf7dff6cbf8a3841
Vlada Dorozhko <dorozhko2v@gmail.com>
2021-07-15 09:48:14 +0300

    little refactor

--- app/src/main/java/com/example/xcriticalapp/MainActivity.kt
+++ app/src/main/java/com/example/xcriticalapp/MainActivity.kt
@@ -28,12 +28,12 @@ class MainActivity : AppCompatActivity() {
         "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])"
     )
 
-    lateinit var viewModel: MainViewModel
+    lateinit var viewModel: MyViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
         initListeners()
         Log.d("activityLiveCycleTest","onCreate")
     }
@@ -66,10 +66,6 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         Log.d("activityLiveCycleTest2","onStart")
     }
-//
-//    private fun validationEmail(email: String?):Boolean{
-//        return email.isNullOrEmpty()||!emailAddressPattern.matcher(email).matches()
-//    }
 
     private fun initListeners() {
 
@@ -90,14 +86,6 @@ class MainActivity : AppCompatActivity() {
             {
                 wrongPassword.visibility=View.VISIBLE
             }
-//            if(validationEmail(email.text.toString()))
-//            {
-//                wrongEmail.visibility=View.VISIBLE
-//            }
-//            else if(!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches())
-//            {
-//                wrongEmail.visibility=View.VISIBLE
-//            }
         }
 
         registrationButton.setOnClickListener {
--- app/src/main/java/com/example/xcriticalapp/MyViewModel.kt
+++ app/src/main/java/com/example/xcriticalapp/MyViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.regex.Pattern
 
-class MainViewModel:ViewModel() {
+class MyViewModel:ViewModel() {
 
     var emailadress = ""
     var viewpassword = ""
@@ -13,9 +13,7 @@ class MainViewModel:ViewModel() {
     private val emailAddressPattern = Pattern.compile(
         "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])"
     )
-//    init{
-//        validationEmail("ok")
-//    }
+
     fun validationEmail():Boolean{
         return emailadress.isNullOrEmpty()||!emailAddressPattern.matcher(emailadress).matches()
     }
@@ -23,19 +21,5 @@ class MainViewModel:ViewModel() {
     fun validationPassword():Boolean{
         return viewpassword.isNullOrEmpty()
     }
-
-//            if(password.text.isNullOrEmpty())
-//            {
-//                wrongPassword.visibility= View.VISIBLE
-//            }
-//            if(email.text.isNullOrEmpty()||!validationEmail(email.text.toString()))
-//            {
-//                wrongEmail.visibility= View.VISIBLE
-//            }
-//            else if(!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches())
-//            {
-//                wrongEmail.visibility=View.VISIBLE
-//            }
-
 
 }
