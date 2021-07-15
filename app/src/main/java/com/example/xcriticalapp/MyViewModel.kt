b364a86d8630155f22819912c9bdd60232c3e5b8
Vlada Dorozhko <dorozhko2v@gmail.com>
2021-07-15 10:53:55 +0300

    Add ViewModel in Fragment
    Take list which we transfer to the Adapter with using ViewModel

--- app/src/main/java/com/example/xcriticalapp/MyViewModel.kt
+++ app/src/main/java/com/example/xcriticalapp/MyViewModel.kt
@@ -1,14 +1,26 @@
 package com.example.xcriticalapp
 
-import android.view.View
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.xcriticalapp.adapter.ExampleItem
 import java.util.regex.Pattern
 
 class MyViewModel:ViewModel() {
 
     var emailadress = ""
     var viewpassword = ""
+
+    fun generateDummyList(size: Int): ArrayList<ExampleItem> {
+        val list = ArrayList<ExampleItem>()
+        for (i in 0 until size) {
+            val drawable = when (i % 2) {
+                0 -> R.drawable.ic_recycle_icon
+                else -> R.drawable.ic_star
+            }
+            val item = ExampleItem("BATUSD" , "CocaCola", "73,2391","/","73,5678","0,31%")
+            list += item
+        }
+        return list
+    }
 
     private val emailAddressPattern = Pattern.compile(
         "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])"
--- app/src/main/java/com/example/xcriticalapp/fragment/MyDealsFragment.kt
+++ app/src/main/java/com/example/xcriticalapp/fragment/MyDealsFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.xcriticalapp.MyViewModel
 import com.example.xcriticalapp.adapter.Adapter
 import com.example.xcriticalapp.adapter.ExampleItem
 import com.example.xcriticalapp.R
@@ -22,22 +26,10 @@ class MyDealsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val model: MyViewModel by viewModels()
         val recyclerView = getView()?.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView?.layoutManager = LinearLayoutManager(this.context)
-        recyclerView?.adapter = Adapter(generateDummyList(15))
-    }
-
-    private fun generateDummyList(size: Int): ArrayList<ExampleItem> {
-        val list = ArrayList<ExampleItem>()
-        for (i in 0 until size) {
-            val drawable = when (i % 2) {
-                0 -> R.drawable.ic_recycle_icon
-                else -> R.drawable.ic_star
-            }
-            val item = ExampleItem("BATUSD" , "CocaCola", "73,2391","/","73,5678","0,31%")
-            list += item
-        }
-        return list
+        recyclerView?.adapter = Adapter(model.generateDummyList(15))
     }
 
 }
