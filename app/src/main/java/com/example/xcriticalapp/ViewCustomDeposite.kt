64cd0a79bc1b349bfe8a33e0a35d09ea5f4a7833
Vlada Dorozhko <dorozhko2v@gmail.com>
2021-07-23 13:04:21 +0300

    Work version with customView

--- app/src/main/java/com/example/xcriticalapp/ViewCustomDeposite.kt
+++ app/src/main/java/com/example/xcriticalapp/ViewCustomDeposite.kt
@@ -1,10 +1,12 @@
 package com.example.xcriticalapp
 
 import android.content.Context
+import android.util.AttributeSet
 import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class ViewCustomDeposite(context: Context?) : LinearLayout(context) {
-    init {
-        inflate(context,R.layout.view_custom_deposite,this)
-    }
+class ViewCustomDeposite(context: Context, attrs: AttributeSet?):ConstraintLayout(context,attrs)  {
+   init {
+       inflate(context, R.layout.view_custom_deposite, this)
+   }
 }
